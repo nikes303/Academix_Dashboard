91ef70a3789c87d44747bfc763efc605993f6a2f
nikes303 <151457253+nikes303@users.noreply.github.com>
2025-06-26 19:21:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 // In server.js, add this line after app.use(express.json());
 
-app.use(express.static('frontend'));
+
 
 // --- 3. FILE UPLOAD (MULTER) CONFIGURATION ---
 // This setup is now in one clean block.
